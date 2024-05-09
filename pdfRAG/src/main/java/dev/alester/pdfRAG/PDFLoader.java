a031747ef6cba96ff498f459930d79c5af7f8606
Alester <alester465@gmail.com>
2024-05-09 18:09:00 -0400

    initial commit for GPT-4 RAG example using spring-ai

---
+++ pdfRAG/src/main/java/dev/alester/pdfRAG/PDFLoader.java
@@ -0,0 +1,56 @@
+package dev.alester.pdfRAG;
+
+import org.springframework.core.io.Resource;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class PDFLoader implements ApplicationRunner {
+    private static final Logger log = LoggerFactory.getLogger(PDFLoader.class);
+    private final JdbcClient jdbcClient;
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/docs/iamblichus-theologyarithmetic_text.pdf")
+    private Resource pdfResource;
+
+    public PDFLoader(JdbcClient jdbcClient, VectorStore vectorStore) {
+        this.jdbcClient = jdbcClient;
+        this.vectorStore = vectorStore;
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Integer count = jdbcClient.sql("select count(*) from vector_store")
+                .query(Integer.class)
+                .single();
+        log.info("Current count of the Vector Store: {}", count);
+        if (count == 0) {
+            log.info("Loading Spring Boot Reference PDF into Vector Store");
+            var config = PdfDocumentReaderConfig.builder()
+                    .withPageExtractedTextFormatter(new ExtractedTextFormatter.Builder().withNumberOfBottomTextLinesToDelete(0)
+                            .withNumberOfTopPagesToSkipBeforeDelete(0)
+                            .build())
+                    .withPagesPerDocument(1)
+                    .build();
+
+            var pdfReader = new PagePdfDocumentReader(pdfResource, config);
+            var textSplitter = new TokenTextSplitter();
+            vectorStore.accept(textSplitter.apply(pdfReader.get()));
+
+            log.info("Application is ready");
+        }
+    }
+}
---
+++ pdfRAG/src/test/java/dev/alester/pdfRAG/PdfRagApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alester.pdfRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfRagApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
